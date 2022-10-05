521f6a1a6d8f47503d305a5e003834599b73abb6
Peter Duong At Mipela <peterduongatmipela@Peters-MacBook-Pro.local>
2022-10-05 09:23:31 +0700

    Added the todo module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'todo',
+    loadChildren: () =>
+      import('./todo/todo.module').then((m) => m.TodoModule),
+  },
+  {
+    path: '',
+    redirectTo: 'todo',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
